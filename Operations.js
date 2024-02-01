ccb8ec9d5942c27ca1c48d7f4997ebd70896ef59
Nabina Mallik <nabinamallik2003@gmail.com>
2024-02-01 21:37:31 +0530

    new

---
+++ Operations.js
@@ -0,0 +1,28 @@
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue); // -3
+
+// console.log(2+2); // 4
+// console.log(2-2); // 0
+// console.log(2*2); // 4
+// console.log(2**3); // 8
+// console.log(2/2); // 1
+// console.log(2%2); // 0
+
+console.log( 1 + "2"); // 12
+console.log("1" + 2); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+console.log("1" + 2 + "2"); // 122
+console.log(1 + "2" + 2); // 122
+
+console.log(true); // true
+console.log(+true); // 1
+console.log(+""); // 0
+ 
+
+let gamecounter = 100;
+++gamecounter;
+
+console.log(gamecounter); // 101
