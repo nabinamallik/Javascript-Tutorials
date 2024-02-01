e55823ae62b499e6ab7a95e92f226889cb118798
Nabina Mallik <nabinamallik2003@gmail.com>
2024-02-01 21:38:45 +0530

    new

---
+++ datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// alert("hello") //we are using node js not broswer
+
+console.log(3
+    +
+    3); // code readability should be high
+
+console.log('nabina');
+
+
+let name = "nabina"
+let age = 18
+let isLogged = false
+
+// number => 2 to 53
+// bigint 
+// string => ""
+// boolean => true/flase
+// null => standalone value
+// undefined => not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined)
