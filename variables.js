23d389548d48269073ac4c825e253bacb349b5fa
Nabina Mallik <nabinamallik2003@gmail.com>
2024-02-01 21:35:52 +0530

    new

---
+++ variables.js
@@ -0,0 +1,19 @@
+const accId = 14455; //constant
+let accEmail = "nabina@google.com";
+var accPass = "12345";
+accCity = "Bhubaneswar"; //also possible
+
+//accId = 2; // not allowed
+
+accEmail = "mallk@google";
+accPass = "20003";
+accCity = "Bhuban";
+let accState;
+
+console.log(accEmail);
+
+
+console.table([accId, accEmail, accPass, accCity. accState]);
+
+// prefer not to use var
+// because of issue in block scope and function scope
